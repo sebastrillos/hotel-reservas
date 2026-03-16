ead5dff870bb30e3820301d54e804b774cbbc883
sebastian trillos <trillossebastian148@gmail.com>
2026-03-16 15:18:44 -0500

    new commit

--- app/Models/Pago.php
+++ app/Models/Pago.php
@@ -1,10 +1,20 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Pago extends Model
 {
-    //
+    protected $table = 'pagos';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'reserva_id', 'monto', 'metodo_pago',
+        'referencia', 'fecha_pago', 'registradopor'
+    ];
+
+    public function reserva()
+    {
+        return $this->belongsTo(Reserva::class, 'reserva_id');
+    }
 }
